168ea4bec865bf52c558bd18637cb6663b19f9b6
BaiYu <1258360186@qq.com>
2023-04-14 16:30:57 +0800

    little css in questionList

---
+++ src/pages/List.tsx
@@ -0,0 +1,27 @@
+import React,{FC,useState} from "react";
+import QuestionCard from "../components/QuestionCard";
+import styles from './List.module.scss'
+
+const List:FC = ()=>{
+    const [list,setlist] = useState([
+        {_id:'q1',title:'问卷1',isPublished:false,isStar:true,answerCount:3,createAt:'3月10日 13:23'},
+        {_id:'q2',title:'问卷2',isPublished:true,isStar:false,answerCount:2,createAt:'3月10日 13:23'},
+        {_id:'q3',title:'问卷3',isPublished:false,isStar:true,answerCount:1,createAt:'3月10日 13:23'}
+    ])
+
+return <>
+ <div className={styles.header}>
+ <div className={styles.left}>我的问卷</div>
+ <div className={styles.right}>搜索</div>
+ </div>
+ <div className={styles.content}>
+    {list.map(q=>{
+        const {_id} = q
+        return <QuestionCard key={_id} {...q}/>
+    })}
+ </div>
+ <div className={styles.footer}>footer</div>
+</>
+}
+
+export default List 
